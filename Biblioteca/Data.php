a8310038741d257bfb3f91f10eefbc651984c24d
gilsonsantiago <gs.thiago@hotmail.com>
2023-08-05 00:31:00 +0000

    Definição de uso do aplicativo

--- Biblioteca/Data.php
+++ Biblioteca/Data.php
@@ -2,32 +2,72 @@
 
 class Data {
 
-  private static $data;
-  private static $dia;
-  private static $mes;
-  private static $ano;
-
-  public static function __constructor()
-  {
-
-    $this->data = '00/00/0000';
-
-    return $this->data;
-     
-  }
-
-  public static function dataToStr($data)
-  {
-
-    
-  }
-
-  public static function strToData($data)
-  {
-
-    
-  }
-
+    private static $data;
+    private static $dia;
+    private static $mes;
+    private static $ano;
   
+    /***************************************************************/
+    public static function __constructor()
+    {
+  
+      self::$data = '00/00/0000';
+  
+      return self::$data;
+       
+    }
+  
+    /***************************************************************/
+    public static function getData()
+    {
+  
+      return self::$data;
+    
+    }
+  
+    /***************************************************************/ 
+    public static function setData()
+    {
+  
+  
+      
+    }  
+  
+    /***************************************************************/ 
+    public static function dataToStr($data)
+    {
+  
+      
+    }
+  
+    /***************************************************************/ 
+    public static function strToData($data)
+    {
+  
+      
+    }
+  
+    /***************************************************************/ 
+    public static function getDiaData()
+    {
+  
+      
+    
+    }
+  
+    /***************************************************************/ 
+    public static function getMesData()
+    {
+  
+       
+      
+    }  
+  
+    /***************************************************************/ 
+   public static function getAnoData()
+   {
+  
+      
+   }   
   
 }
